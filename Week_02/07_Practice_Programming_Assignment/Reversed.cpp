9c3087193a32260db6debf88c69f514b7a2654ae
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-02-13 10:11:11 +0300

    Add files via upload

---
+++ Week_02/07_Practice_Programming_Assignment/Reversed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> Reversed(const vector<int>& v) {
+	vector<int> result;
+	for (int i = v.size() - 1; i >= 0; --i) result.push_back(v[i]);
+	return result;
+}
+int main()
+{
+	vector<int> numbers = { 1, 5, 3, 4, 2 };
+	numbers = Reversed(numbers);
+	for (auto num : numbers) {
+		cout << num << " ";
+	}
+	return 0;
+}
