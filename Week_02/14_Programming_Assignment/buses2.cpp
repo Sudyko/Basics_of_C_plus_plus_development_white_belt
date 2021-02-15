9f4f8ead6daa8f007815a18e2a62d8e576d593de
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-02-16 00:04:04 +0300

    Add files via upload

---
+++ Week_02/14_Programming_Assignment/buses2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	map<int, vector<string>> base;
+	for (n; n > 0; --n) {
+		vector<string> list_of_stops;
+		int arg1;
+		string arg2;
+		cin >> arg1;
+		for (arg1; arg1 > 0; --arg1) {
+			cin >> arg2;
+			list_of_stops.push_back(arg2);
+		}
+		arg1 = 0;
+		int i = 0;
+		for (i; i < base.size(); ++i) {
+			if (base[i] == list_of_stops) {
+				arg1 += 1;
+				break;
+			}	
+		}
+		if (arg1 != 0)
+			cout << "Already exists for " << i + 1 << endl;
+		else {
+			arg1 = base.size();
+			cout << "New bus " << arg1 + 1 << endl;
+			base[arg1] = list_of_stops;
+		}
+	}
+	return 0;
+}
