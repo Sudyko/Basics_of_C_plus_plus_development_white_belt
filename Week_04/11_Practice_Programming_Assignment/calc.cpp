47323ee832a327a5f4e9af29a08ac8302046537f
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-03-11 10:52:22 +0300

    Add files via upload

---
+++ Week_04/11_Practice_Programming_Assignment/calc.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+int evcl(const int& x, const int& y) {
+    if (y == 0)
+        return x;
+    return evcl(y, x % y);
+}
+int FindNok(const int& a, const int& b) {
+    return a * b / evcl(a, b);
+}
+class Rational {
+public:
+    Rational() {}
+    Rational(int a, int b) {
+        if (b == 0) {
+            throw invalid_argument("Invalid argument");
+        }
+        int nod = abs(evcl(a, b));
+        if (a < 0 && b < 0) {
+            numerator = -a / nod;
+            denominator = -b / nod;
+        }
+        else if (a >= 0 && b < 0) {
+            numerator = -a / nod;
+            denominator = -b / nod;
+        }
+        else {
+            numerator = a / nod;
+            denominator = b / nod;
+        }
+    }
+    int Numerator() const {
+        return numerator;
+    }
+
+    int Denominator() const {
+        return denominator;
+    }
+
+private:
+    int numerator = 0;
+    int denominator = 1;
+};
+Rational operator + (const Rational& lhs, const Rational& rhs) {
+    int nok = FindNok(lhs.Denominator(), rhs.Denominator());
+    if (lhs.Denominator() > nok) {
+        int new_numerator = lhs.Denominator() / nok * lhs.Numerator() +
+            rhs.Denominator() / nok * rhs.Numerator();
+        return Rational(new_numerator, nok);
+    }
+    else {
+        int new_numerator = nok / lhs.Denominator() * lhs.Numerator() +
+            nok / rhs.Denominator() * rhs.Numerator();
+        return Rational(new_numerator, nok);
+    }
+}
+Rational operator - (const Rational& lhs, const Rational& rhs) {
+    int nok = FindNok(lhs.Denominator(), rhs.Denominator());
+    if (lhs.Denominator() > nok) {
+        int new_numerator = lhs.Denominator() / nok * lhs.Numerator() -
+            rhs.Denominator() / nok * rhs.Numerator();
+        return Rational(new_numerator, nok);
+    }
+    else {
+        int new_numerator = nok / lhs.Denominator() * lhs.Numerator() -
+            nok / rhs.Denominator() * rhs.Numerator();
+        return Rational(new_numerator, nok);
+    }
+}
+Rational operator * (const Rational& lhs, const Rational& rhs) {
+    return Rational(lhs.Numerator() * rhs.Numerator(),
+        lhs.Denominator() * rhs.Denominator());
+}
+Rational operator / (const Rational& lhs, const Rational& rhs) {
+    if (rhs.Numerator() == 0) {
+        throw domain_error("Division by zero");
+    }
+    return lhs * Rational(rhs.Denominator(), rhs.Numerator());
+}
+bool operator == (const Rational& lhs, const Rational& rhs) {
+    return lhs.Numerator() == rhs.Numerator() && lhs.Denominator() == rhs.Denominator();
+}
+bool operator < (const Rational& lhs, const Rational& rhs) {
+    return (lhs - rhs).Numerator() < 0;
+}
+ostream& operator << (ostream& stream, const Rational& r) {
+    return stream << r.Numerator() << '/' << r.Denominator();
+}
+istream& operator >> (istream& stream, Rational& r) {
+    int a, b;
+    char c;
+    stream >> a >> c >> b;
+    if (stream && c == '/') {
+        r = Rational(a, b);
+    }
+    return stream;
+}
+int main() {
+    Rational r1, r2;
+    char operation;
+    try {
+        cin >> r1 >> operation >> r2;
+        if (operation == '+') {
+            cout << r1 + r2 << endl;
+        }
+        else if (operation == '-') {
+            cout << r1 - r2 << endl;
+        }
+        else if (operation == '*') {
+            cout << r1 * r2 << endl;
+        }
+        else if (operation == '/') {
+            try {
+                cout << r1 / r2 << endl;
+            }
+            catch (domain_error& de) {
+                cout << de.what();
+            }
+        }
+    }
+    catch (invalid_argument& ia) {
+        cout << ia.what();
+    }
+    return 0;
+}
